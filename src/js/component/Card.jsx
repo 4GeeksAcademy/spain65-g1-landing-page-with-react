a5f98bb535aa5983979efc5365bea919f3edc275
Victoria González Rodríguez <159072629+Vickygonrod@users.noreply.github.com>
2024-04-15 18:34:50 +0000

    First commit card

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="card-group">
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Card;
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Card from "./component/Card.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Card />, document.querySelector("#app"));
